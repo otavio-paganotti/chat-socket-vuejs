98773970be0b1612b31f7c7bd06b9944c4f0edf8
Otávio Paganotti <paganottiotavio@gmail.com>
2020-04-05 19:15:22 -0400

    atualizando o repo

--- backend/sockets/base.js
+++ backend/sockets/base.js
@@ -1,23 +1,37 @@
 const socketIO = io => {
+
   io.on( 'connection',  socket => {
-    console.log( 'New user Connected: ' + socket.id )
+
+    console.log( 'New user Connected: ' + socket.id );
+
     socket.username = 'anonymous';
+
     socket.on('user', (name) => {
-      socket.username = name
+      socket.username = name;
       console.log(name + ' Acabou de entrar no chat');
     });
+
     socket.on('message', (msg) => {
-      io.emit('message', { 'user': socket.username, 'message': msg, 'identifier': socket.id })
-      console.log({ 'user': socket.username, 'message': msg, 'identifier': socket.id })
+
+      io.emit('message', { 'user': socket.username, 'message': msg, 'identifier': socket.id });
+
+      console.log({ 'user': socket.username, 'message': msg, 'identifier': socket.id });
+
     });
+
     socket.on('join_user', (username) => {
+      
       if (username != null) {
-        socket.username = username
+        socket.username = username;
       }
+
       socket.broadcast.emit('message',
-      { 'user': 'Server', 'message': socket.username + ' has joined!'})
+      { 'user': 'Server', 'message': socket.username + ' has joined!'});
+
     });
-  } )
+    
+  });
+
 }
 
 module.exports = socketIO
